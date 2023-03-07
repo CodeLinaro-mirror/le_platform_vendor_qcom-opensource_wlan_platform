c21f07b255421b1d4914065e549d802f43b48821
Jingxiang Ge <quic_jge@quicinc.com>
2023-03-07 15:13:40 +0800

    cnss2: set mhi segment length from dts
    Support to config mhi segment length from dts node.
    current only SZ512K and SZ256K support.
    
    Change-Id: I100e09aea25d4f4d70cf11428044841d007fc92c
    CRs-Fixed: 3434750

--- cnss2/main.h
+++ cnss2/main.h
@@ -497,6 +497,7 @@ struct cnss_plat_data {
 	char fw_build_id[QMI_WLFW_MAX_BUILD_ID_LEN + 1];
 	u32 otp_version;
 	u32 fw_mem_seg_len;
+	u32 mhi_seg_len;
 	struct cnss_fw_mem fw_mem[QMI_WLFW_MAX_NUM_MEM_SEG_V01];
 	struct cnss_fw_mem m3_mem;
 	struct cnss_fw_mem *cal_mem;
